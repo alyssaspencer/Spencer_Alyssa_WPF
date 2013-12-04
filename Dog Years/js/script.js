f552d830e60093a6271ec4e4eb156c8d433ae164
alyssa.spencer <alyssa.spencer@fullsail.edu>
2013-12-04 14:49:53 -0500

    Finding out how old Sparky is in dog years

---
+++ Dog Years/js/script.js
@@ -0,0 +1,9 @@
+//Calculate dog years based on human years
+var humanYears = 7;
+var dogYears;
+
+//Dog years equals actual years times 7
+dogYears = humanYears * 7;
+
+//Print out the age in dog years
+console.log("Sparky is " + humanYears + " human years old which means he is " + dogYears + " in dog years");
