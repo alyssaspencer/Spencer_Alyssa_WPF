d65efac41a525d0b697a83845b4a6dacca236c32
alyssa.spencer <alyssa.spencer@fullsail.edu>
2013-12-04 14:50:57 -0500

    This is how many slices of pie per person there are.

---
+++ Slice_of_Pie Pt.1/js/script.js
@@ -0,0 +1,11 @@
+//Calculate the number of slices of pizza each person at the party will get
+var slicesPerPizza = 12;
+var numPeople = 14;
+var numPizza = 5;
+var slicesPerPerson;
+
+//multiply number of pizzas times number of slices then divide by the number of people
+slicesPerPerson = slicesPerPizza * numPizza / numPeople;
+
+//print out the slices per person
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
